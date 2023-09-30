cf0dd77a977318c112869131beacf8f018127af0
Aylin <A01379272@itesm.mx>
2023-09-29 23:38:57 -0600

    SetUp MCV .net

---
+++ Prueba_2/Web_Aplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web_Aplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
